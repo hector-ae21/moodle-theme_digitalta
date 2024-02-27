87384e55737115206a955ee212d42089e47300b3
Franklin Alejando López Ramírez <franklin.lopez@ct.uneatlantico.es>
2024-02-27 15:04:01 +0100

    first commit base

---
+++ lang/es/theme_dta.php
@@ -0,0 +1,43 @@
+<?php
+
+/**
+ * dta theme
+ *
+ * @package    theme_dta
+ * @copyright  2024 ADSDR-FUNIBER Scepter Team
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+// Esta línea protege el archivo de ser accedido directamente a través de una URL.
+defined('MOODLE_INTERNAL') || die();
+// El nombre de la segunda pestaña en los ajustes del tema.
+$string['advancedsettings'] = 'Ajustes avanzados';
+// La configuración del color de marca.
+$string['brandcolor'] = 'Color de marca';
+// Descripción de la configuración del color de marca.
+$string['brandcolor_desc'] = 'El color de acento.';
+// Una descripción mostrada en el selector de tema del administrador.
+$string['choosereadme'] = 'El tema DTA es un tema hijo de Boost. Añade la capacidad de cargar un fondo DTA.';
+// Nombre de las páginas de ajustes.
+$string['configtitle'] = 'Ajustes de DTA';
+// Nombre de la primera pestaña de ajustes.
+$string['generalsettings'] = 'Ajustes generales';
+// El nombre de nuestro plugin.
+$string['pluginname'] = 'DTA';
+// Configuración de archivos predefinidos.
+$string['presetfiles'] = 'Archivos predefinidos de tema adicionales';
+// Texto de ayuda para archivos predefinidos.
+$string['presetfiles_desc'] = 'Los archivos predefinidos pueden ser utilizados para alterar drásticamente la apariencia del tema. Consulta <a href=https://docs.moodle.org/dev/Boost_Presets>Boost presets</a> para obtener información sobre cómo crear y compartir tus propios archivos predefinidos, y consulta el <a href=http://moodle.net/boost>Repositorio de predefinidos</a> para obtener predefinidos compartidos por otros.';
+// Configuración de predefinido.
+$string['preset'] = 'Predefinido del tema';
+// Texto de ayuda para predefinido.
+$string['preset_desc'] = 'Elige un predefinido para cambiar ampliamente el aspecto del tema.';
+// Configuración de SCSS crudo.
+$string['rawscss'] = 'SCSS crudo';
+// Texto de ayuda para SCSS crudo.
+$string['rawscss_desc'] = 'Utiliza este campo para proporcionar código SCSS o CSS que se inyectará al final de la hoja de estilos.';
+// Configuración de SCSS crudo inicial.
+$string['rawscsspre'] = 'SCSS crudo inicial';
+// Texto de ayuda para SCSS crudo inicial.
+$string['rawscsspre_desc'] = 'En este campo puedes proporcionar código SCSS de inicialización, se inyectará antes de todo lo demás. La mayoría de las veces usarás esta configuración para definir variables.';
+ // Necesitamos incluir una cadena de idioma para cada región
