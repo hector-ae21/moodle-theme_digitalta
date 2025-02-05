f7facab2fc70a9d5d720760f61e9332fedb15024
Eduardo Estrada <me@e2rd0.com>
2025-02-05 11:14:06 +0100

    fix / update plugin version

--- version.php
+++ version.php
@@ -24,7 +24,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = '2024120300';
+$plugin->version = '2025020400';
 $plugin->component = 'theme_digitalta';
 $plugin->requires = '2022112800';
 $plugin->dependencies = [
