06fb551fc36c725299885d581c867ab4fea623b0
Hector Arrechea <hectorlazaroarrechea@gmail.com>
2025-02-12 17:49:08 +0100

    Update version.php

--- version.php
+++ version.php
@@ -24,7 +24,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = '2025020400';
+$plugin->version = '2025020720';
 $plugin->component = 'theme_digitalta';
 $plugin->requires = '2022112800';
 $plugin->dependencies = [
