3bb674878a82b06d6ae9f933180f362dd863467b
Eduardo Estrada <me@e2rd0.com>
2025-03-10 10:39:55 +0100

    fix / changed external images urls

--- version.php
+++ version.php
@@ -24,7 +24,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = '2025022400';
+$plugin->version = '2025031000';
 $plugin->component = 'theme_digitalta';
 $plugin->requires = '2022112800';
 $plugin->dependencies = [
